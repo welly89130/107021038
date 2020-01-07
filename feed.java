95ab74a8b8e709155c6ea21ab43c58d6a5d34bba
welly89130 <43198505+welly89130@users.noreply.github.com>
2020-01-07 13:31:51 +0800

    Update feed.java

--- feed.java
+++ feed.java
@@ -12,23 +12,23 @@ public class feed extends JLabel implements Runnable{
         Random rand = new Random();
         rand.setSeed(System.currentTimeMillis());
         icons[0]= new ImageIcon("feed.png");
-       
+
         x = rand.nextInt(720)+50;
         y = rand.nextInt(400)+50;
         z = rand.nextInt(3);
         System.out.println("z = "+ z);
 
         this.setBounds(x, y, 300, 400);
-        
+
         Image img1 = icons[0].getImage();
         Image img2 = img1.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
         icons[0].setImage(img2);
 
-        
+
 
         this.setIcon(icons[z]);
 
-        t1 = new Timer((rand.nextInt(5)+1)) * 100, new ActionListener(){
+        t1 = new Timer((rand.nextInt(5)+1) * 100, new ActionListener(){
             public void actionPerformed(ActionEvent ae){
                 if(flag){
                     if(y+5 >= 820){
