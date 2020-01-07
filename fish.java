0c9676417b413f74abdc81ddaf8b17e7dbdff922
welly89130 <43198505+welly89130@users.noreply.github.com>
2020-01-07 13:32:27 +0800

    Update fish.java

--- fish.java
+++ fish.java
@@ -3,12 +3,12 @@ import java.awt.*;
 import java.util.Random;
 import java.awt.event.*;
 
-public class Fish extends JLabel implements Runnable{
+public class fish extends JLabel implements Runnable{
     private ImageIcon icons[] = new ImageIcon [3];
     private Timer t1;
     private int x, y, z;
     private Boolean flag = false;
-    public Fish() {
+    public fish() {
         Random rand = new Random();
         rand.setSeed(System.currentTimeMillis());
         icons[0]= new ImageIcon("fish1.png");
@@ -21,7 +21,7 @@ public class Fish extends JLabel implements Runnable{
         System.out.println("z = "+ z);
 
         this.setBounds(x, y, 300, 400);
-        
+
         Image img1 = icons[0].getImage();
         Image img2 = img1.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
         icons[0].setImage(img2);
@@ -36,7 +36,7 @@ public class Fish extends JLabel implements Runnable{
 
         this.setIcon(icons[z]);
 
-        t1 = new Timer((rand.nextInt(5)+1)) * 100, new ActionListener(){
+        t1 = new Timer((rand.nextInt(5)+1) * 100, new ActionListener(){
             public void actionPerformed(ActionEvent ae){
                 if(flag){
                     if(x+5 >= 820){
@@ -51,7 +51,7 @@ public class Fish extends JLabel implements Runnable{
                         x -= 5;
                     }
                 }
-                Fish.this.setLocation(x, y);
+                fish.this.setLocation(x, y);
             }
         });
     }
