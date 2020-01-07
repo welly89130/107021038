0be26d9b9ecdac02908e72419f3844ca9c011a22
welly89130 <43198505+welly89130@users.noreply.github.com>
2020-01-07 16:11:41 +0800

    Update fish.java

--- fish.java
+++ fish.java
@@ -3,17 +3,15 @@ import java.awt.*;
 import java.util.Random;
 import java.awt.event.*;
 
-public class fish extends JLabel implements Runnable{
+public class feed extends JLabel implements Runnable{
     private ImageIcon icons[] = new ImageIcon [3];
     private Timer t1;
     private int x, y, z;
     private Boolean flag = false;
-    public fish() {
+    public feed () {
         Random rand = new Random();
         rand.setSeed(System.currentTimeMillis());
-        icons[0]= new ImageIcon("fish1.png");
-        icons[1]= new ImageIcon("fish2.png");
-        icons[2]= new ImageIcon("fish3.png");
+        icons[0]= new ImageIcon("feed.jpg");
 
         x = rand.nextInt(720)+50;
         y = rand.nextInt(400)+50;
@@ -26,32 +24,26 @@ public class fish extends JLabel implements Runnable{
         Image img2 = img1.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
         icons[0].setImage(img2);
 
-        img1 = icons[1].getImage();
-        img2 = img1.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
-        icons[1].setImage(img2);
 
-        img1 = icons[2].getImage();
-        img2 = img1.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
-        icons[2].setImage(img2);
 
         this.setIcon(icons[z]);
 
         t1 = new Timer((rand.nextInt(5)+1) * 100, new ActionListener(){
             public void actionPerformed(ActionEvent ae){
                 if(flag){
-                    if(x+5 >= 820){
+                    if(y+5 >= 820){
                         flag = false;
                     }else {
-                        x += 5;
+                        y += 5;
                     }
                 }else{
-                    if(x-5 < 0){
+                    if(y-5 < 0){
                         flag = true;
                     }else {
-                        x -= 5;
+                        y -= 5;
                     }
                 }
-                fish.this.setLocation(x, y);
+                feed.this.setLocation(x, y);
             }
         });
     }
